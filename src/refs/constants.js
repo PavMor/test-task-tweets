be06f617caa842468c66e05fb44b9564c4f017ce
Your Name <pavlo.morozenko@ukr.net>
2023-06-03 17:21:16 +0200

    url api_imgs_theme adding

---
+++ src/refs/constants.js
@@ -0,0 +1,18 @@
+export const statusFilters = ['Show all', 'Follow', 'Followings'];
+
+export const routesList = [
+  { name: 'Main', path: '/' },
+  { name: 'Tweets', path: '/tweets' },
+];
+
+export const lsKeys = Object.freeze({
+  users: 'users',
+  filter: 'filter',
+  followings: 'followings',
+});
+
+export const limit = 9;
+
+export const totalItems = 90;
+
+export const baseUrlApi = 'https://64667a502ea3cae8dc180276.mockapi.io/:e';
